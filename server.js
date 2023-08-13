4f70e551b629d28c2b6374ae2237f45a4d99afe5
ari hlfgut <ari@0541234.com>
2023-08-13 17:11:42 +0300

    exercise fetch new

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
+//exercise 1
 const reqOptions = {
   method: "get",
   headers: {
     "Content-Type": "application/json",
   },
 };
-
 const name1 = document.getElementById("name");
 const email1 = document.getElementById("email");
 const img1 = document.getElementById("img");
 const age1 = document.getElementById("age");
 
-const buttonApi = document.getElementById("buttonApi");
-buttonApi.addEventListener("click", () => {
+const buttonApi1 = document.getElementById("buttonApi1");
+buttonApi1.addEventListener("click", () => {
   getApi().then((data) => {
     name1.textContent =
       data.results[0].name.title +
@@ -19,8 +19,9 @@ buttonApi.addEventListener("click", () => {
       data.results[0].name.first +
       " " +
       data.results[0].name.last;
+
     email1.textContent = data.results[0].email;
-    img1.src = data.results[0].picture.large;
+    img1.src = data.results[0].picture.medium;
     age1.textContent = data.results[0].dob.age;
   });
 });
@@ -33,3 +34,27 @@ async function getApi() {
     console.log(error);
   }
 }
+
+//exercise 2
+const buttonApi2 = document.getElementById("buttonApi2");
+buttonApi2.addEventListener("click", () => {
+  getApi().then((data) => {
+    console.log(data);
+  });
+});
+async function getApi() {
+  try {
+    const user = await fetch(
+      "https://randomuser.me/api?results=5&gender=male",
+      reqOptions
+    );
+    const dataUser = await user.json();
+    return dataUser;
+  } catch {
+    console.log(error);
+  }
+}
+
+//exercise 3
+
+/////ergr/r/r//
